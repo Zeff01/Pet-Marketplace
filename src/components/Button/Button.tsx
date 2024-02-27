ad99d32f0ef0947cde42a352285ecad390af0e0b
tavs <82376053+7iquid@users.noreply.github.com>
2024-02-27 21:53:40 +0800

    modify button for jest update lint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Image, Text, ActivityIndicator, ButtonProps } from 'react-native';
+import { Pressable, Image, Text, ActivityIndicator, ButtonProps } from 'react-native';
 import { IButton } from './Button.typeDefs';
 
 export default function Button({
@@ -16,14 +16,18 @@ export default function Button({
 }: IButton & ButtonProps) {
   const opacityStyle = { opacity: disabled ? 0.6 : 1 };
   return (
-    <TouchableOpacity
-      style={[opacityStyle, style]}
-      activeOpacity={activeOpacity ?? 0.2}
-      disabled={disabled ?? isLoading}
-      {...rest}>
-      {isLoading && <ActivityIndicator size="small" color={loaderColor} />}
-      {!isLoading && image && <Image source={image} style={imageStyle} />}
-      {!isLoading && title && <Text style={titleStyle}>{title}</Text>}
-    </TouchableOpacity>
+    <Pressable
+      style={({ pressed }) => [{ opacity: pressed || disabled ? 0.6 : 1 }, style]}
+      onPress={!disabled ? rest.onPress : undefined}>
+      {({ pressed }) => (
+        <>
+          {isLoading && <ActivityIndicator size="small" color={loaderColor} />}
+          {!isLoading && image && <Image source={image} style={imageStyle} />}
+          {!isLoading && title && (
+            <Text style={[titleStyle, { opacity: pressed || disabled ? 0.6 : 1 }]}>{title}</Text>
+          )}
+        </>
+      )}
+    </Pressable>
   );
 }
