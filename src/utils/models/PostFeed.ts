d8431aa0ecc907df97b8faec04c06414b3f201c5
7iquid <taviefalcon@gmail.com>
2024-02-29 21:03:24 +0800

    intigrate front-end and back-end

--- src/utils/models/PostFeed.ts
+++ src/utils/models/PostFeed.ts
@@ -3,12 +3,14 @@ import { type Timestamp } from 'firebase/firestore';
 export type DocId = string;
 
 export interface PostFeeds {
-  user_name: string;
-  pet_id: DocId;
-  heart_count?: number;
-  share_count?: number;
-  main_photo: string;
-  created_at: Timestamp;
-  comment?: DocId;
+  caption?: string;
+  displayName?: string;
   comment_count: number;
+  comment?: DocId;
+  created_at: Timestamp;
+  heart_count?: number;
+  main_photo: string;
+  pet_id: DocId;
+  share_count?: number;
+  user_name: string;
 }
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { Text, View, StyleSheet, StatusBar } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import Button from '@components/Button';
 import { StackProps } from '@navigator/stack';
 import { colors } from '@theme';
+import { PostFeedContext } from '../../providers/PostFeedsProvider';
 
+const Home: React.FC = () => {
+  const { postFeeds } = useContext(PostFeedContext);
+  // console.log('running Home----------------1', postFeeds);
+  return (
+    <View>
+      <Text>Home Page</Text>
+      {postFeeds?.map((postFeed, index) => (
+        <View key={index}>
+          <Text>{postFeed.user_name}</Text>
+          {/* Use Image component to display the image */}
+          <Image source={{ uri: postFeed.main_photo }} style={{ width: 200, height: 200 }} />
+          {/* Render other post feed data */}
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default Home;
 const styles = StyleSheet.create({
   root: {
     flex: 1,
@@ -28,20 +48,3 @@ const styles = StyleSheet.create({
     backgroundColor: colors.lightPurple,
   },
 });
-
-export default function Home({ navigation }: StackProps) {
-  return (
-    <View style={styles.root}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Home</Text>
-      <Button
-        title="Go to Details"
-        titleStyle={styles.buttonTitle}
-        style={styles.button}
-        onPress={() => {
-          navigation.navigate('DetailsStack', { from: 'Home' });
-        }}
-      />
-    </View>
-  );
-}
